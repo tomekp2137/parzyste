885e25c5c78fd126942a8ecc22aff1cf71409c99
tomekp2137 <127328323+tomekp2137@users.noreply.github.com>
2023-03-21 21:35:53 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+
+public static void main(String[] args) {
+	int i=0;
+	System.out.println("Pętla for");
+	 for (i=2;i<=100;i++) {
+		 if (i%2==0) {
+			 System.out.println(i);
+		 }
+	 }
+}
+}
